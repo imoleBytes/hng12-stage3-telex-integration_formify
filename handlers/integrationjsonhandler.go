96ac5860b5fd4266c344bf44c037403678d342ec
imoleFolio <dremkay71@gmail.com>
2025-02-18 12:58:52 +0100

    fix: live url added to integration json

--- handlers/integrationjsonhandler.go
+++ handlers/integrationjsonhandler.go
@@ -1,8 +1,6 @@
 package handlers
 
 import (
-	"os"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -24,7 +22,7 @@ var Data = IntegrationStruct{
 		AppName:         "Formify FaaS",
 		AppDescription:  "Formify is a seamless Form as a Service (FaaS) tool that empowers users to collect and manage form data without the need for a backend infrastructure. With Formify, users can simply embed a URL into the 'action' attribute of an HTML form, and the submitted data will be automatically processed and sent to a designated Telex channel.\n\nGenerate unique action attribbute value for your HTML Forms",
 		AppLogo:         "https://img.freepik.com/free-vector/retro-circular-pattern-design_1308-175051.jpg?t=st=1739808712~exp=1739812312~hmac=f03b43859fc31fbdd675f1907599bd626959e488186958f208d879c6fd1ef10a&w=740",
-		AppURL:          os.Getenv("SERVER_ADDR"),
+		AppURL:          "https://hng12-stage3-telex-integration-formify.onrender.com",
 		BackgroundColor: "#fff",
 	},
 	IntegrationCategory: "CRM & Customer Support",
@@ -38,7 +36,7 @@ var Data = IntegrationStruct{
 	},
 	Author:    "Imoleayo Kolawole",
 	Settings:  []Setting{{Label: "Form Name", Type: "text", Default: "", Required: true}, {Label: "Website", Type: "text", Default: "", Required: true}},
-	TargetURL: os.Getenv("SERVER_ADDR") + "/generate-formify",
+	TargetURL: "https://hng12-stage3-telex-integration-formify.onrender.com/generate-formify",
 }
 
 // this returns the integration json
