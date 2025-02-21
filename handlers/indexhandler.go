86a07cc7c67fe5c84aa5cb6189ae068d5ba4031c
imoleFolio <dremkay71@gmail.com>
2025-02-21 15:37:17 +0100

    fix: index route restrucured to return details about the integration instead of redirecting to the README

--- handlers/indexhandler.go
+++ handlers/indexhandler.go
@@ -8,5 +8,20 @@ import (
 
 // handles the index route
 func HandleIndex(ctx *gin.Context) {
-	ctx.Redirect(http.StatusTemporaryRedirect, "https://github.com/imoleBytes/hng12-stage3-telex-integration_formify/blob/main/README.md")
+	ctx.JSON(http.StatusOK, gin.H{
+		"status":      "success",
+		"status_code": 200,
+		"data": struct {
+			Integration string
+			Description string
+			Author      string
+			GithubRepo  string
+		}{
+			Integration: "Formify FaaS",
+			Description: "Formify is a seamless Form as a Service (FaaS) tool that empowers users to collect and manage form data without the need for a backend infrastructure. With Formify, users can simply embed a URL into the 'action' attribute of an HTML form, and the submitted data will be automatically processed and sent to a designated Telex channel.\n\nGenerate unique action attribbute value for your HTML Forms",
+			Author:      "Imoleayo Kolawole",
+			GithubRepo:  "https://github.com/telexintegrations/hng12-stage3-telex-integration_formify",
+		},
+	})
+	// ctx.Redirect(http.StatusTemporaryRedirect, "https://github.com/imoleBytes/hng12-stage3-telex-integration_formify/blob/main/README.md")
 }
